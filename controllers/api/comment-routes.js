641ccab94cba149bb70571b1273c10b3fffb4677
alex <alexreveles@yahooo.com>
2021-03-19 20:51:48 -0700

    partials update

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -46,4 +46,16 @@ router.get('/', (req, res) => {
   
   module.exports = router;
   
-  
+//   const router = require("express").Router();
+// const { Comment } = require("../../models/");
+// const withAuth = require("../../utils/auth");
+// router.post("/", withAuth, (req, res) => {
+//   Comment.create({ ...req.body, userId: req.session.userId })
+//     .then(newComment => {
+//       res.json(newComment);
+//     })
+//     .catch(err => {
+//       res.status(500).json(err);
+//     });
+// });
+// module.exports = router;
