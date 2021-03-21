d3ad875d3e8d4ed9fb7974b5d6a91f921a2a1e90
alex <alexreveles@yahooo.com>
2021-03-20 22:12:44 -0700

    update routes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const router = require('./controllers/index');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -31,9 +32,11 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(router);
 
 // app.use(require('./controllers/'));
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 }); 
+
